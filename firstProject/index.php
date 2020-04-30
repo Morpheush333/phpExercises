544d71de5f28c5865749f252357ec03c725b705b
Mateusz Medon <mateuszmedon1@gmail.com>
2020-04-30 16:15:53 +0100

    first commit

---
+++ firstProject/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Pharmacy</title>
+</head>
+<body>
+    
+    <h1>Pharmacy Order Form <h1>
+    <form action="order.php">
+        Aspirine (2,99 Euro/pack)
+        <input type="text"/>
+        </br></br>
+        Mask (8,99 Euro/pack x12)
+        <input type="text"/>
+        </br></br>
+        <input type="submit" value="sent order">
+    </form> 
+    
+</body>
+</html>
---
+++ firstProject/order.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    
+<?php
+
+    $name = "Mateusz";
+    echo "$name, hey one the web";
+
+?>
+
+    
+</body>
+</html>
