e7897209a5feab1ee39a78460b6e5daf69378e32
Mateusz Medon <mateuszmedon1@gmail.com>
2020-04-30 16:49:41 +0100

    print summary value in order.php file

--- firstProject/order.php
+++ firstProject/order.php
@@ -2,12 +2,35 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Pharmacy</title>
+    <title>Pharmacy order collection</title>
 </head>
 <body>
 
-    $aspirine = 
+<?php
 
+    $aspirine = $_POST['aspirine'];
+    $mask = $_POST['mask'];
+    $result = $aspirine * 2.99 + $mask * 8.99; 
+
+echo<<<END
+    
+    <h2>Summary order <h2>
+    <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+        <td>Aspirine (2,99 Euro/each) </td><td>$aspirine </td>
+    </tr>
+    <tr>
+        <td>Mask (8,99 Euro/pack x12) </td><td>$mask </td>
+    </tr>
+    <tr>
+        <td>To pay </td> <td> $result Euro</td>
+    </tr>
+    </table>
+
+
+END;
+
+?>
     
 </body>
 </html>
